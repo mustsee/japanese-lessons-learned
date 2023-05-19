9831b9f41ba56a98c12b654b4636cbc534cb20b7
mustsee <mustsee@users.noreply.github.com>
2023-05-19 06:11:54 +0000

    deploy: 9f9cd3777ac01a1cde1160a309ff559eb623b175

---
+++ assets/guide_grammaire_phrase.md.76beb2a8.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o as a,a as i}from"./app.70463bee.js";const _=JSON.parse(`{"title":"La formation d'une phrase","description":"","frontmatter":{},"headers":[],"relativePath":"guide/grammaire/phrase.md"}`),n={name:"guide/grammaire/phrase.md"},r=i("",8),t=[r];function l(o,p,u,d,c,m){return a(),s("div",null,t)}var f=e(n,[["render",l]]);export{_ as __pageData,f as default};
